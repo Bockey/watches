a83ace0022fedf7daae9cf678828a73a9e7fc85c
Bockey <71261529+Bockey@users.noreply.github.com>
2021-03-19 21:26:39 +0100

    day name and date

--- js/digital.js
+++ js/digital.js
@@ -1,8 +1,13 @@
 const hoursContainer = document.querySelector(".hours");
 const minutesContainer = document.querySelector(".minutes");
 const secondsContainer = document.querySelector(".seconds");
+const dateContainer = document.querySelector(".date");
+const dayContainer = document.querySelector(".day");
 
 const time = new Date();
+let day = time.getDay();
+let date = time.getUTCDate();
+
 let hours = time.getHours();
 let minutes = time.getMinutes();
 let seconds = time.getSeconds();
@@ -26,6 +31,7 @@ function moveTime() {
   hoursContainer.innerHTML = hours;
   minutesContainer.innerHTML = minutes;
   secondsContainer.innerHTML = seconds;
+  dateContainer.textContent = date;
   if (hours < 10) {
     hoursContainer.innerHTML = "0" + hours;
   }
@@ -34,6 +40,28 @@ function moveTime() {
   }
   if (seconds < 10) {
     secondsContainer.innerHTML = "0" + seconds;
+  }
+  /*day name change*/
+  if (day === 1) {
+    dayContainer.textContent = "MO";
+  }
+  if (day === 2) {
+    dayContainer.textContent = "TU";
+  }
+  if (day === 3) {
+    dayContainer.textContent = "WE";
+  }
+  if (day === 4) {
+    dayContainer.textContent = "TH";
+  }
+  if (day === 5) {
+    dayContainer.textContent = "FR";
+  }
+  if (day === 6) {
+    dayContainer.textContent = "SA";
+  }
+  if (day === 7) {
+    dayContainer.textContent = "SU";
   }
 }
 
