2b6078110763af2fd5e52cd54b0cb7e3d64b9a45
Bockey <71261529+Bockey@users.noreply.github.com>
2021-03-20 23:57:11 +0100

    mobile version

--- js/light.js
+++ js/light.js
@@ -2,6 +2,7 @@ const turnOn = document.querySelector(".pressLight");
 const lightSwitch = document.querySelector(".light");
 const screen = document.querySelector(".screen");
 const hold = document.querySelector(".hold");
+var mediaQ = window.matchMedia("(max-width: 675px)");
 
 turnOn.addEventListener("mousedown", function () {
   screen.style.backgroundColor = "rgb(3, 116, 18)";
@@ -13,3 +14,22 @@ turnOn.addEventListener("mouseup", function () {
   lightSwitch.style.left = "-20";
   hold.style.display = "none";
 });
+
+/*meida query */
+
+function myFunction(mediaQ) {
+  if (mediaQ.matches) {
+    // If media query matches
+    turnOn.addEventListener("touchstart", function () {
+      screen.style.backgroundColor = "rgb(3, 116, 18)";
+      lightSwitch.style.left = "-4";
+      hold.style.display = "block";
+    });
+    turnOn.addEventListener("touchend", function () {
+      screen.style.backgroundColor = "rgb(153, 160, 145)";
+      lightSwitch.style.left = "-15";
+      hold.style.display = "none";
+    });
+  }
+}
+myFunction(mediaQ);
